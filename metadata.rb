687dc733e6e4e0d2dc58e1813e63ff35e9b99d73
Issac Goldstand <margol@beamartyr.net>
2014-10-05 14:46:44 +0300

    fix depends

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'margol@beamartyr.net'
 license          'All rights reserved'
 description      'Installs/Configures newrelic-sysmond-policy startup/shutdown scripts'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
-depends 'nodejs', '~> 2.0.0'
-depends 'newrelic', '~> 2.0.0'
+depends 'nodejs'
+depends 'newrelic'
