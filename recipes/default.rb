ae3da828a863c8e94f0fdb9e022ad2a862eb2e65
Issac Goldstand <margol@beamartyr.net>
2014-10-22 11:56:31 +0300

    centos fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,8 +18,14 @@ log "NewRelic API key not defined.  Not enabling the service!" do
 end
 
 service "newrelic-sysmond-policy" do
-   supports :start => true, :stop => true, :restart => false
+   supports :start => true, :stop => true, :restart => false, :status => false
    priority 90
    action [:enable, :start]
    only_if {node[:newrelic][:apikey]}
+end
+
+# Workaround RH/Chef issues described at https://github.com/opscode/chef/issues/2168
+execute "newrelic-sysmond-policy-service-centosfix" do
+  command "chkconfig newrelic-sysmond-policy reset && service newrelic-sysmond-policy start"
+  only_if {node[:newrelic][:apikey] && platform_family?("rhel")}
 end
