efd83b8c3e9be804de7e647e177ed38ba7708cfd
Issac Goldstand <margol@beamartyr.net>
2014-10-22 12:05:29 +0300

    update supported platforms

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,10 @@ maintainer_email 'margol@beamartyr.net'
 license          'Apache v2.0'
 description      'Installs/Configures newrelic-sysmond-policy startup/shutdown scripts'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 provides "service[newrelic-sysmond-policy]"
+platform 'ubuntu'
+platform 'centos'
 
 depends 'nodejs'
 recommends 'newrelic'
